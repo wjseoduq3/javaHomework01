f5a169d46162ef412dd4d43b7b05f0e3fabc6ca4
wjseoduq3 <dragon65@nate.com>
2024-04-30 22:48:40 +0900

    1'st commit

---
+++ src/javaHomework01/Helloworld_atHome.java
@@ -0,0 +1,12 @@
+package javaHomework01;
+
+public class Helloworld_atHome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!");
+		System.out.println("안녕하세요.");
+
+	}
+
+}
